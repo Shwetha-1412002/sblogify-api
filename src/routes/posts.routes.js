d3fbadce950211cbe7f1846b5884c6dd189455f8
Shwetha <shwethaaradhya81@gmail.com>
2026-02-12 15:16:16 +0530

    feat: complete module 3 database integration

--- src/routes/posts.routes.js
+++ src/routes/posts.routes.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const postController = require('../controllers/posts.controller');
 
-router.get('/', postController.getAllPosts);
-router.get('/:id', postController.getPostById);
+// Create Post
 router.post('/', postController.createPost);
+
+// Get All Posts
+router.get('/', postController.getAllPosts);
+
+// Get One Post
+router.get('/:id', postController.getPostById);
+
+// Update Post
+router.patch('/:id', postController.updatePost);
+
+// Delete Post
+router.delete('/:id', postController.deletePost);
 
 module.exports = router;
